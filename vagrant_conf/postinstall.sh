443902382ece19a2c188a3682147d8df8e3c05ed
letsdodev <repolevedp@gmail.com>
2025-11-24 11:12:03 +0100

    modified as final version

--- vagrant_conf/postinstall.sh
+++ vagrant_conf/postinstall.sh
@@ -1,22 +1,19 @@
 #!/bin/bash
-export USERID=peter
-export USERNAME="Mr.Peter"
-export USERPASSWD="VMware2025"
+subscription-manager register --username $SUB_USERNAME_VAR --password $SUB_PASSWORD_VAR
 dnf update -y
 dnf install fish git tree -y
+timedatectl set-local-rtc 0
+export USERID=zorro
+export USERNAME="Mr.Zorro"
+export USERPASSWD="BMW#2025"
 useradd --comment $USERNAME --shell $(which fish) $USERID
 echo $USERID:$USERPASSWD | chpasswd
 passwd --expire $USERID
 mkdir -pv /home/$USERID/.ssh
 chmod -vR u=rwX /home/$USERID/.ssh
 chmod -vR og-rwx /home/$USERID/.ssh
-echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMJ2LKMyBUhLACMAiuMdBShSJidchM+LLzw45G3jL/Td SyslogClient02 created on 2025-11-01" > /home/$USERID/.ssh/authorized_keys
-chown $USERID:$USERID /home/$USERID/.ssh
-
-#mkdir -pv /home/$USERID/.vim/colors
+echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIO5wL3QVIe+yi+0+ILK7LZBQMq7n+ez46mD7a75oL31p 2025-11-23" > /home/$USERID/.ssh/authorized_keys
+chown -Rv $USERID:$USERID /home/$USERID/.ssh
 mount -o ro /tmp/VBoxGuestAdditions.iso /mnt
 bash /mnt/VBoxLinuxAdditions.run
-
-
-
-
+systemctl shutdown -r now
